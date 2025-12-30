84bc1839b7482b1b8fe3d3b186b09699b1c02c92
Eimen2018 <nur.amuni26@gmail.com>
2025-12-31 03:04:18 +0400

    Add IGolfViewer3D framework files for iOS x86_64 simulator
    - Created Info.plist for IGolfViewer3D framework with necessary metadata.
    - Added module.modulemap to define the framework module and export headers.
    - Included CodeResources for code signing, detailing header files and their hashes.

---
+++ example/ios/IGolfViewer3D.xcframework/ios-x86_64-simulator/IGolfViewer3D.framework/Headers/MeasurementSystem.h
@@ -0,0 +1,17 @@
+//
+//  MeasurementSystem.h
+//  iGolfViewer3D
+//
+//  Copyright (c) 2024. iGolf, Inc. - All Rights Reserved.
+//  You may use this code under the terms of the license.
+//
+
+#ifndef MeasurementSystem_h
+#define MeasurementSystem_h
+
+typedef NS_ENUM(NSUInteger, MeasurementSystem) {
+    MeasurementSystemImperial,
+    MeasurementSystemMetric
+};
+
+#endif
---
+++ ios/IGolfViewer3D.xcframework/ios-x86_64-simulator/IGolfViewer3D.framework/Headers/IGolfViewer3D.h
@@ -0,0 +1,31 @@
+//
+//  IGolfViewer3D.h
+//  IGolfViewer3D
+//
+//  Copyright (c) 2024. iGolf, Inc. - All Rights Reserved.
+//  You may use this code under the terms of the license.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for IGolfViewer3D.
+FOUNDATION_EXPORT double IGolfViewer3DVersionNumber;
+
+//! Project version string for IGolfViewer3D.
+FOUNDATION_EXPORT const unsigned char IGolfViewer3DVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <IGolfViewer3D/PublicHeader.h>
+
+#import <IGolfViewer3D/CourseRenderView.h>
+#import <IGolfViewer3D/CourseRenderViewLoader.h>
+#import <IGolfViewer3D/NavigationMode.h>
+#import <IGolfViewer3D/MeasurementSystem.h>
+#import <IGolfViewer3D/CalloutsDrawMode.h>
+#import <IGolfViewer3D/TextureQuality.h>
+#import <IGolfViewer3D/OpenGLESRenderView.h>
+#import <IGolfViewer3D/VEndpoint.h>
+#import <IGolfViewer3D/V3DShape.h>
+#import <IGolfViewer3D/V3DCircle.h>
+#import <IGolfViewer3D/V3DLine.h>
+#import <IGolfViewer3D/V3DPolygon.h>
+#import <IGolfViewer3D/V3DPolyline.h>
