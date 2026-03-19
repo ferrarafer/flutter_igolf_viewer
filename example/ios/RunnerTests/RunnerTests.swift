70dbfad7deb79060466eb34cb1b60beb3ca0fbe1
Eimen2018 <nur.amuni26@gmail.com>
2026-03-20 02:49:18 +0400

    chore: drop example test changes from ios fix pr

--- example/ios/RunnerTests/RunnerTests.swift
+++ example/ios/RunnerTests/RunnerTests.swift
@@ -1,90 +1,26 @@
+import Flutter
+import UIKit
 import XCTest
-import IGolfViewer3D
 
 @testable import flutter_igolf_viewer
 
-final class RunnerTests: XCTestCase {
+// This demonstrates a simple unit test of the Swift portion of this plugin's implementation.
+//
+// See https://developer.apple.com/documentation/xctest for more information about using XCTest.
 
-  func testSameHoleSameModeIsNoOp() {
-    let plan = HoleTransitionModePlanner.makePlan(
-      currentHole: 4,
-      currentMode: .modeFreeCam,
-      requestedHole: 4,
-      requestedMode: .modeFreeCam
-    )
+class RunnerTests: XCTestCase {
 
-    XCTAssertTrue(plan.isNoOp)
-    XCTAssertFalse(plan.shouldUpdateHole)
-    XCTAssertFalse(plan.shouldApplyModeImmediately)
-    XCTAssertFalse(plan.shouldResetPostLoadModeFlag)
-    XCTAssertNil(plan.pendingModeAfterHoleLoad)
+  func testGetPlatformVersion() {
+    let plugin = FlutterIgolfViewerPlugin()
+
+    let call = FlutterMethodCall(methodName: "getPlatformVersion", arguments: [])
+
+    let resultExpectation = expectation(description: "result block must be called.")
+    plugin.handle(call) { result in
+      XCTAssertEqual(result as! String, "iOS " + UIDevice.current.systemVersion)
+      resultExpectation.fulfill()
+    }
+    waitForExpectations(timeout: 1)
   }
 
-  func testHoleChangeToFreeCamAppliesImmediatelyAndReappliesAfterLoad() {
-    let plan = HoleTransitionModePlanner.makePlan(
-      currentHole: 4,
-      currentMode: .modeFlyover,
-      requestedHole: 5,
-      requestedMode: .modeFreeCam
-    )
-
-    XCTAssertFalse(plan.isNoOp)
-    XCTAssertTrue(plan.shouldUpdateHole)
-    XCTAssertTrue(plan.shouldApplyModeImmediately)
-    XCTAssertTrue(plan.shouldResetPostLoadModeFlag)
-    XCTAssertEqual(
-      plan.pendingModeAfterHoleLoad,
-      PendingHoleChangeNavigationMode(hole: 5, mode: .modeFreeCam)
-    )
-
-    let resolution = HoleTransitionModePlanner.resolveModeAfterHoleLoad(
-      loadedHole: 5,
-      pendingMode: plan.pendingModeAfterHoleLoad,
-      hasAppliedPostLoadMode: false
-    )
-
-    XCTAssertEqual(resolution, .applyPending(.modeFreeCam))
-  }
-
-  func testHoleChangeToFlyoverDefersImmediateApplyAndReappliesAfterLoad() {
-    let plan = HoleTransitionModePlanner.makePlan(
-      currentHole: 5,
-      currentMode: .modeFreeCam,
-      requestedHole: 6,
-      requestedMode: .modeFlyover
-    )
-
-    XCTAssertFalse(plan.isNoOp)
-    XCTAssertTrue(plan.shouldUpdateHole)
-    XCTAssertFalse(plan.shouldApplyModeImmediately)
-    XCTAssertTrue(plan.shouldResetPostLoadModeFlag)
-    XCTAssertEqual(
-      plan.pendingModeAfterHoleLoad,
-      PendingHoleChangeNavigationMode(hole: 6, mode: .modeFlyover)
-    )
-
-    let resolution = HoleTransitionModePlanner.resolveModeAfterHoleLoad(
-      loadedHole: 6,
-      pendingMode: plan.pendingModeAfterHoleLoad,
-      hasAppliedPostLoadMode: false
-    )
-
-    XCTAssertEqual(resolution, .applyPending(.modeFlyover))
-  }
-
-  func testHoleLoadWithoutPendingModeUsesBootstrapFreeCamOnce() {
-    let firstResolution = HoleTransitionModePlanner.resolveModeAfterHoleLoad(
-      loadedHole: 1,
-      pendingMode: nil,
-      hasAppliedPostLoadMode: false
-    )
-    XCTAssertEqual(firstResolution, .applyBootstrapFreeCam)
-
-    let secondResolution = HoleTransitionModePlanner.resolveModeAfterHoleLoad(
-      loadedHole: 1,
-      pendingMode: nil,
-      hasAppliedPostLoadMode: true
-    )
-    XCTAssertEqual(secondResolution, .keepCurrentMode)
-  }
 }
